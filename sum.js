cbdaab983c22b1540daf9c5b445eb975e3b4ee09
RanaMuhammadIrshad <rmirshad@web.de>
2022-06-08 16:05:34 +0200

    task is done

---
+++ sum.js
@@ -0,0 +1,14 @@
+// Create a function sum which accepts any amount of numbers and return the sum of their addition
+
+const sum = (...arg) => {
+  let sum = 0;
+
+  for (let i = 0; i < arg.length; i++) {
+    sum += arg[i];
+  }
+  return sum;
+};
+
+console.log(sum(1)); // -> 1
+console.log(sum(1, 15)); // -> 16
+console.log(sum(25, 25, 20)); // -> 70
---
+++ twofer.js
@@ -0,0 +1,10 @@
+// Complete the function below so that it returns "Two for me and one for you" when no arguments are passed
+
+const twofer = (who) => {
+  if (typeof who === "string") {
+    return `Two for me and one for ${who}`;
+  } else return `Two for me and one for ${(who = "you")}`;
+};
+
+console.log(twofer("Fran")); // -> "Two for me and one for Fran"
+console.log(twofer()); // -> "Two for me and one for you"
