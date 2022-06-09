1781152c029a9a4b8cf2e22c5651e0925aa5c1e1
RanaMuhammadIrshad <rmirshad@web.de>
2022-06-09 10:53:14 +0200

    some changes in variable names

--- twofer.js
+++ twofer.js
@@ -1,5 +1,5 @@
 // Complete the function below so that it returns "Two for me and one for you" when no arguments are passed
-console.log(`-----twofer-----`);
+console.log(`-----twofer-Method-1-----`);
 const twofer = (who) => {
   if (typeof who === "string") {
     return `Two for me and one for ${who}`;
@@ -8,3 +8,14 @@ const twofer = (who) => {
 
 console.log(twofer("Fran")); // -> "Two for me and one for Fran"
 console.log(twofer()); // -> "Two for me and one for you"
+
+
+console.log(`-----twofer-Method-2-----`);
+const twofer2 = (who = 'you') => {
+  
+    return `Two for me and one for ${who}`;
+  
+};
+
+console.log(twofer2("Fran")); // -> "Two for me and one for Fran"
+console.log(twofer2()); // -> "Two for me and one for you"
